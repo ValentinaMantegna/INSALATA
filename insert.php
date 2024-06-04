3ac7f23883d59240e894fc3d3f73ed8e7656113f
LucaGaddi <leaque@hotmail.it>
2024-06-04 14:47:56 +0200

    insertguasto

--- insert.php
+++ insert.php
@@ -19,15 +19,38 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['prezzo']) && isset($_
     $prezzo = $_POST['prezzo'];
     $tipo = $_POST['tipo'];
 
+
+    // Preparazione della query di selezione
+    $queryControlloOfferta = "SELECT * FROM item WHERE prezzo <= $prezzo AND tipo = 'O'";
+    $queryControlloDomanda = "SELECT * FROM item WHERE prezzo <= $prezzo AND tipo = 'D'";
+
+
+    // Esecuzione della query
+    $resultOfferta = $conn->query($queryControlloOfferta);
+    $resultDomanda = $conn->query($queryControlloOfferta);
+
+    // Controllo se il dato è già presente nel database
+    if ($tipo = 'D' && $resultOfferta->num_rows > 0) {
+    echo "Domanda possibile!";
+    }
+    // Controllo se il dato è già presente nel database
+    if ($tipo = 'O' && $resultDomanda->num_rows > 0) {
+        echo "Offerta possibile!";
+        }
+    else {
+
     // Preparazione della query di inserimento
     $query = "INSERT INTO item (quantita, prezzo, tipo) VALUES (1, $prezzo, '$tipo')";
+    
+    }
 
     // Esecuzione della query
     if ($conn->query($query) === TRUE) {
-        echo "Nuovo dato inserito con successo!";
+        echo "<br> successo!";
     } else {
         echo "Errore durante l'inserimento del dato: " . $conn->error;
     }
+    
 }
 
 // Chiusura della connessione
