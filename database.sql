3406f75b2980a47efff500f67acc183fb3b8da71
LucaGaddi <leaque@hotmail.it>
2024-06-04 09:35:55 +0200

    db

---
+++ database.sql
@@ -0,0 +1,15 @@
+-- Creazione del database
+CREATE DATABASE insalata;
+
+-- Selezione del database
+USE insalata;
+
+CREATE TABLE vendite (
+    id INT AUTO_INCREMENT,
+    prodotto VARCHAR(50),
+    domanda_quantita INT,
+    domanda_prezzo DECIMAL(10, 2),
+    offerta_quantita INT,
+    offerta_prezzo DECIMAL(10, 2),
+    PRIMARY KEY (id)
+    );
---
+++ view.php
@@ -0,0 +1,32 @@
+<?php
+
+// Configurazione della connessione
+$host = 'localhost';
+$username = 'root';
+$password = 'Sw2023';
+$dbname = 'insalata';
+
+// Creazione della connessione
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Controllo della connessione
+if ($conn->connect_error) {
+    die("Errore di connessione: " . $conn->connect_error);
+}
+
+// Esecuzione di una query
+$query = "SELECT * FROM tabella";
+$result = $conn->query($query);
+
+// Elaborazione dei risultati
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo $row['colonna1'] . " " . $row['colonna2'] . "\n";
+    }
+} else {
+    echo "Nessun risultato";
+}
+
+// Chiusura della connessione
+$conn->close();
+?>
